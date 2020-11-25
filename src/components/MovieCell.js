a7a35b4f21df3c9154a7593da4702affbe1f99dd
Eduardo M <emartinslemos@gmail.com>
2020-11-24 23:10:43 -0400

    esqueleto base - tela de seleção

---
+++ src/components/MovieCell.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function MovieCell({titulo, ano}) {
+  console.log(titulo)
+  return (
+    <div className="cell" >
+      <div
+        style={{ margin: 16 }}>
+        <input
+          name="isGoing"
+          type="checkbox" />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }} >
+        <text style={{ fontWeight: 'bold' }}>{titulo}</text>
+        <text>{ano}</text>
+      </div>
+    </div>
+  )
+}
