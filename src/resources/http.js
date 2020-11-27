b841899fdc0599436151dd558cadb67acb78dfd0
Eduardo M <emartinslemos@gmail.com>
2020-11-27 15:18:30 -0400

    Services com header

--- src/resources/http.js
+++ src/resources/http.js
@@ -1,6 +1,9 @@
 import Axios from 'axios'
 import {BASE_URL} from './urls'
 
-const http = Axios.create({baseURL: BASE_URL})
+const http = Axios.create({
+  baseURL: BASE_URL,
+  headers: {"Access-Control-Allow-Origin": "*"}
+})
 
 export {http}
