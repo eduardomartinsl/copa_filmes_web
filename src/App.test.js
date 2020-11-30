23126c1305a7b118a6f8a021ab62f43d84787ad1
Eduardo M <emartinslemos@gmail.com>
2020-11-30 18:23:11 -0400

    Testes em raiz

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   // const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renderiza titulo de Campeonato de filmes', () => {
+  render(<App />);
+  const titleElement = screen.getByText(/Campeonato de filmes/)
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('Renderiza descrição de primeira etapa de caompeonato de filmes', () => {
+  render(<App/>)
+  const descricaoElement = screen.getByText('Selecione 8 filmes que você deseja que entrem na competição e depois pressione o botão gerar meu campeonato para prosseguir');
+  expect(descricaoElement).toBeInTheDocument()
+})
+
+test('renderiza lista de filmes localmente', () => {
+  render(<App/>)
+  // const descricaoElement = screen.getByText('Selecione 8 filmes que você deseja que entrem na competição e depois pressione o botão gerar meu campeonato para prosseguir');
+  const botaoElement = screen.getByText(/Gerar meu campeonato/);
+  expect(botaoElement).toBeInTheDocument()
+})
